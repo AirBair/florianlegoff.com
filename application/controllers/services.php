a01922a3b7204a70cef8d8c1c54ad42667b444cb
Krachoris <krachoris@a.jupimail.com>
2014-12-02 21:52:51 +0100

    Mise en bdd et fix du css de la page service

--- application/controllers/services.php
+++ application/controllers/services.php
@@ -8,12 +8,20 @@
  	public function __construct()
  	{
  		parent::__construct();
+ 		$this->load->model('projets_model');
  	}
 
 
  	public function index()
  	{
- 		$data['titre'] = 'Services | Florian LE GOFF';
+ 		$data = array(
+ 			'titre' => 'Services | Florian LE GOFF',
+ 			'propose' => $this->projets_model->getService('propose'),
+ 			'but' => $this->projets_model->getService('but'),
+ 			'avantages' => $this->projets_model->getService('avantages'),
+ 			'informations' => $this->projets_model->getService('informations')
+ 		);
+
  		$this->load->view('services/services', $data);
  	}
  }
