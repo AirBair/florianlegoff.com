6fea02e8f3c91d817b4e302d9ef7d0e6a3fd4768
Krachoris <contact@florianlegoff.com>
2015-08-22 16:03:18 -0400

    New Design + Form in AJAX on Contact page

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -7,57 +7,62 @@ class Contact extends CI_Controller
 
 	public function index()
 	{
+          $data = array(
+               'titre' => 'Contact | Florian LE GOFF',
+               'description' => 'Des questions ? Des propositions ? Contactez-moi ! - Florian LE GOFF'
+          );
+		$this->load->view('contact', $data);
+     }
 
-		$nom = htmlentities($this->input->post('nom'));
-		$expediteur = $this->input->post('mail');
-		$message = nl2br(htmlentities($this->input->post('message')));
-          $objet = htmlentities($this->input->post('objet'));
+     public function send()
+     {
 
-          // Règles à implémenter
-          $this->form_validation->set_rules('nom', 'Nom', 'trim|required');
-          $this->form_validation->set_rules('mail', 'E-mail', 'trim|required|valid_email', array('valid_email' => 'Vous devez rentrez une adresse email valide'));
-          $this->form_validation->set_rules('message', 'Message', 'trim|required');
-          $this->form_validation->set_rules('objet', 'Objet', 'trim|required');
+          if(!$this->input->is_ajax_request()):
+               redirect('contact');exit;
+          endif;
 
-		if ($this->form_validation->run())
+          $this->form_validation->set_rules('nom', 'Nom', 'trim|required', array('required' => 'Le champ "nom" est requis'));
+          $this->form_validation->set_rules('mail', 'E-mail', 'trim|required|valid_email', array('valid_email' => 'Vous devez rentrez une adresse email valide', 'required' => 'Le champ "e-mail" est requis'));
+          $this->form_validation->set_rules('objet', 'Objet', 'trim|required', array('required' => 'Le champ "objet" est requis'));
+          $this->form_validation->set_rules('message', 'Message', 'trim|required', array('required' => 'Le champ "message" est requis'));
+
+          if ($this->form_validation->run())
           {
-                    date_default_timezone_set('Europe/Paris');
+               $nom = htmlentities($this->input->post('nom'));
+               $expediteur = $this->input->post('mail');
+               $objet = htmlentities($this->input->post('objet'));
+               $message = nl2br(htmlentities($this->input->post('message')));
+               
+               date_default_timezone_set('Europe/Paris');
 
-                    // **  I. Envoi de mail  **
-                    $this->load->library('email');
-                    $this->email->from($expediteur, $nom);
-                    $this->email->to('contact@florianlegoff.com');
-                    $this->email->cc('florianlg@cryptolab.net');
-                    $this->email->subject($objet);
-                    $this->email->message($message);
-                    $this->email->send();
+               // **  I. Envoi de mail  **
+               $this->load->library('email');
+               $this->email->from($expediteur, $nom);
+               $this->email->to('contact@florianlegoff.com');
+               $this->email->cc('florianlg@cryptolab.net');
+               $this->email->subject($objet);
+               $this->email->message($message);
+               $this->email->send();
  
  
-                    // **  II. Sauvegarde du mail en base de données.  **
+               // **  II. Sauvegarde du mail en base de données.  **
 
-                    // Mise en tableau des données à insérer
-                    
-                    $data = array(
-                         'nom' => $nom,
-                         'email' => $expediteur,
-                         'objet' => $objet,
-                         'message' => $message,
-                         'date_envoi' => date("Y-m-d H:i:s")
-                    );
-
-                    // Envoi en base de données
-                    $this->admin_model->saveMail($data);
- 
-                    // Affichage de la page de confirmation.
-                    return $this->load->view('contact_confirmation');
-          }
-		else
-		{  
+               // Mise en tableau des données à insérer
                $data = array(
-                    'titre' => 'Contact | Florian LE GOFF',
-                    'description' => 'Des questions ? Des propositions ? Contactez-moi ! - Florian LE GOFF'
+                    'nom' => $nom,
+                    'email' => $expediteur,
+                    'objet' => $objet,
+                    'message' => $message,
+                    'date_envoi' => date("Y-m-d H:i:s")
                );
-			$this->load->view('contact', $data);
-		}
+
+               // Envoi en base de données
+               $this->admin_model->saveMail($data);
+     
+               // Envoi de la confirmation d'execution à la requête AJAX.
+               echo "success";
+          }
+          else
+               echo validation_errors(); // On retourne les erreurs de validations de formulaire à la requête AJAX
      }
 } // Fin de la classe contact.php   
--- application/views/contact.php
+++ application/views/contact.php
@@ -2,28 +2,86 @@
 	
 <iframe width="100%" height="300px" frameBorder="0" src="http://umap.openstreetmap.fr/fr/map/florian-le-goff_49646?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe>
 
-<div class="formContact">
-	<?php
-	if(validation_errors() != null): ?>
-		<div class="formError">
+<div class="conteneur formContact">
+
+		<div class="formError" style="display:none;">
 			<h4>Erreur(s) dans la validation du formulaire :</h4>
-			<?php echo validation_errors(); ?>
+			<div class="errors_validation"></div>
 		</div>
-	<?php endif; ?>
+
+		<div class="formSuccess" style="display:none;">
+			<h4>Votre message à bien été envoyé !</h4>
+			<p>Je vous répondrais au plus vite.</p>
+		</div>
 	
 	<?php echo form_open('contact'); ?>
-			<label for="nom">Votre nom :</label>
-			<input type="text" name="nom" id="nom" value="<?php echo set_value('nom'); ?>" required autofocus /><br />
+			<input type="text" name="nom" id="nom" placeholder="Votre nom" value="<?php echo set_value('nom'); ?>" autofocus /><br />
 			
-			<label for="mail">Votre e-mail :</label>
-			<input type="text" name="mail" id="mail" value="<?php echo set_value('mail'); ?>" required /><br />
+			<input type="text" name="mail" id="mail" placeholder="Votre e-mail" value="<?php echo set_value('mail'); ?>" /><br />
 			
-			<label for="objet">Objet :</label>
-			<input type="text" name="objet" id="objet" value="<?php echo set_value('objet'); ?>" required /><br />
+			<input type="text" name="objet" id="objet" placeholder="Objet du message" value="<?php echo set_value('objet'); ?>" /><br />
 			
-			<textarea name="message" id="message" cols="50" rows="10" required ><?php echo set_value('nom'); ?></textarea><br />
-			<input type="submit" value="Envoyer" />
+			<textarea name="message" id="message" cols="50" rows="10" placeholder="Votre message .." ><?php echo set_value('nom'); ?></textarea><br />
+
+			<input type="submit" id="submit" value="Envoyer" />
 	<?php echo form_close(); ?>
+
+	<div class="infosContact" >
+		<h3>Informations de Contact</h3>
+
+		<p>
+			<span><img src="<?php echo site_url(); ?>assets/images/icones/ico_mail.png" alt="Icone courriel" /> <a href="mailto:contact@florianlegoff.com">contact@florianlegoff.com</a></span>
+			<span><img src="<?php echo site_url(); ?>assets/images/icones/ico_key.png" alt="Icone clé" />  <a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x329F5F50B1294A46" target="_blank">Clé GPG</a></span>
+			<span><img src="<?php echo site_url(); ?>assets/images/icones/ico_tchat.png" alt="Icone discussion" /> <a href="myToxID">TOX</a></span>
+			<span><label>Mon Identifiant</label><textarea rows="1">AF448137C1017AA436CF0833CD55BEB5F306087434A609801466835E7C163811E06796BDBB7B</textarea></span>
+		</p>
+	</div>
 </div>
+
+<script type="text/javascript">
+	$(function(){
+
+		// Envoi du mail en AJAX
+		$('#submit').click(function() {
+
+			var form_data = {
+				nom : $('#nom').val(),
+				mail : $('#mail').val(),
+				objet : $('#objet').val(),
+				message : $('#message').val()
+			};
+
+			$.post("<?php echo site_url('contact/send'); ?>", form_data, function(data){
+				if(data == "success")
+				{
+					$('.formError').hide();
+					$('.formSuccess').hide();
+					$('.formSuccess').slideDown(100);
+
+					// Reset des valeurs du formulaire
+					$('input[type="text"]').each(function(){
+						$(this).val('');
+					})
+					$('form textarea').val('');
+				}
+				else
+				{
+					$('.formSuccess').hide();
+					$('.formError .errors_validation').replaceWith('<div class="errors_validation">'+data+'</div>');
+					$('.formError').slideDown(100);
+				}
+			});
+
+			return false;
+		});
+
+
+		$('a[href="myToxID"').click(function(){
+			$('.infosContact label').fadeIn(50);
+			$('.infosContact textarea').fadeIn(50);
+			return false;
+		});
+	});
+</script>
 
 <?php require('application/views/template/footer.php') ?>
