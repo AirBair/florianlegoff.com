c82deb3dd71f8cd28e58456270219ba67bd97daf
Krachoris <contact@florianlegoff.com>
2015-09-09 14:03:59 -0400

    Update of the skills nav

--- application/views/competences.php
+++ application/views/competences.php
@@ -1,12 +1,12 @@
 <?php require('application/views/template/header.php'); ?>
-	
+
 <div class="banniereSkills">
 	<h3>COMPETENCES</h3>
 </div>
 <div class="skills">
 
 	<p class="openNavCatSkills"><img src="<?php echo site_url(); ?>assets/images/icones/flecheMenu_droit.png" alt="Flèche déroulante" /></p>
-	
+
 	<div class="navCatSkills">
 		<p id="sideBar"><img src="<?php echo site_url(); ?>assets/images/icones/flecheMenu_gauche.png" alt="Flèche déroulante" /></p>
 	<?php foreach ($categories as $categorie): ?>
@@ -41,7 +41,7 @@
 									<a href="<?php echo site_url('competences/edit/'.$competence->id_skill); ?>"><img src="<?php echo site_url(); ?>assets/images/icones/ico_edit.png" alt="Editer" /></a>
 									<a class="delComp" href="<?php echo site_url('competences/delete/'.$competence->id_skill); ?>"><img src="<?php echo site_url(); ?>assets/images/icones/ico_delete.png" alt="Supprimer" /></a>
 								</p>
-							<?php endif; ?>	
+							<?php endif; ?>
 						</div>
 					<?php endforeach; ?>
 					</section>
@@ -52,13 +52,12 @@
 </div>
 
 <script type="text/javascript">
-	
 	$(function(){
 
 		// Suppression d'une compétence en AJAX
 		$('.delComp').each(function() {
 			$(this).on('click', function(){
-				
+
 				if( confirm('Confirmation de suppression ?') )
 				{
 					var url = $(this).attr('href');
@@ -77,7 +76,7 @@
 			});
 		});
 
-		/** Menu Déroulant **/
+		/**** Menu Déroulant ****/
 
 		// Effet au survol
 		$('.openNavCatSkills').mouseenter(function(){
@@ -101,27 +100,36 @@
 		});
 
 		// Suivi du menu dans la page
-		$(window).scroll(function() {
-			var cible = 250 - $(window).scrollTop();
+		function navComp(){
+			var cible = $('.banniereSkills').innerHeight() - $(window).scrollTop();
      		if (cible <= 0)
      		{
-         		$('.navCatSkills').css('top', '0px');
-         		$('.openNavCatSkills').css('top', '0px');
+				$('.navCatSkills').css('top', '0px');
+        		$('.openNavCatSkills').css('top', '0px')
+									  .css('padding-top', (window.innerHeight-$('footer').innerHeight())/2);
          	}
          	else
          	{
          		$('.navCatSkills').css('top', cible+'px');
-         		$('.openNavCatSkills').css('top', cible+'px');
+         		$('.openNavCatSkills').css('top', cible+'px')
+									  .css('padding-top', (window.innerHeight-cible-$('footer').innerHeight())/2);
          	}
+		}
+
+		// Initialisation du menu
+		navComp();
+		// Mise à jour du menu lors du scroll
+		$(window).scroll(function() {
+			navComp();
    		});
 
 		// Scroll à la section demandée
 		$('.navCatSkills p').each(function(){
 	    	$(this).click(function(){
 	        	var ancre = '#section'+$(this).attr('id');
-	         	$('html, body').animate({  
-	            	scrollTop:$(ancre).offset().top - 50 
-	         	}, 'slow'); 
+	         	$('html, body').animate({
+	            	scrollTop:$(ancre).offset().top - 50
+	         	}, 'slow');
 	        	return false;
 	    	});
    		});
