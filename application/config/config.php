af21c952674c41c5727f1345772f4ec85a57df47
Krachoris <contact@florianlegoff.com>
2016-06-11 20:59:36 +0200

    Fix of base_url config for production

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://florianlegoff.tk';
+$config['base_url'] = 'https://www.florianlegoff.com';
 
 /*
 |--------------------------------------------------------------------------
