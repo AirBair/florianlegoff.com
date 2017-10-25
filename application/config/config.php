5a4c480d075e2806e7f608663e3e4447e2f0aa1f
Florian LE GOFF <contact@florianlegoff.com>
2017-10-25 14:21:41 +0200

    Fix base url in config file changed and commited during last local developement

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://florianlegoff.loc';
+$config['base_url'] = 'https://www.florianlegoff.com';
 
 /*
 |--------------------------------------------------------------------------
