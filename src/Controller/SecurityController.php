00d719708f9778caec0df91e4fcfce2ea2dcffb6
Florian LE GOFF <contact@florianlegoff.com>
2018-02-12 22:52:11 +0100

    Configure security component to access admin section

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * Login page
+     *
+     * @Route("/login", name="login")
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     *
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('login.html.twig', array(
+            'username'  => $authenticationUtils->getLastUsername(),
+            'error'     => $authenticationUtils->getLastAuthenticationError(),
+        ));
+    }
+
+    /**
+     * Logout page display if not defined in security firewall
+     *
+     * @Route("/logout", name="logout")
+     *
+     * @throws \RuntimeException
+     */
+    public function logout()
+    {
+        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
+    }
+}
