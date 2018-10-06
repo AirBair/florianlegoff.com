c7e0871f942fef5929934a00abab10fe1a2833bd
Florian LE GOFF <contact@florianlegoff.com>
2018-10-06 18:47:26 +0200

    Adding monolog to dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Translation\Bundle\TranslationBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
