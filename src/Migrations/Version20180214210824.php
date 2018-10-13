6bd7432c0f6abe7fb4038438806fa6c9cfd493ce
Florian LE GOFF <contact@florianlegoff.com>
2018-10-13 18:19:48 +0200

    Fix namespace of deprecated Doctrine AbstractMigration

--- src/Migrations/Version20180214210824.php
+++ src/Migrations/Version20180214210824.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 class Version20180214210824 extends AbstractMigration
 {
