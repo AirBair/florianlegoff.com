469a6a40eaa3ad154e1f1ae1d5009bdfd0a16d2e
Florian LE GOFF <contact@florianlegoff.com>
2020-03-15 16:32:27 +0100

    Upgrade to Symfony 5 (with v5.0.5) & sync recipes

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class LocaleSubscriber implements EventSubscriberInterface
@@ -34,7 +34,7 @@ class LocaleSubscriber implements EventSubscriberInterface
     /**
      * {@inheritdoc}
      */
-    public function onKernelRequest(GetResponseEvent $event): void
+    public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
 
